23b01da1fceb28bc40855721aba13be561e85cab
Jeniffer Martins <martins.jeniffer@usp.br>
2020-07-20 20:28:50 -0300

    ex4: puppeteer runs inside site and takes screenshot of page 2

---
+++ example4.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+const iPhone6 = puppeteer.devices['iPhone 6'];
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.emulate(iPhone6);
+  await page.goto('https://simulador.credihome.com.br/simulador/');
+  
+  await page.click('input#realtyPrice');
+  await page.type('input#realtyPrice', "100000.00");
+  await page.click('input#mortgageValue');
+  await page.type('input#mortgageValue', "60000.00");
+  await page.click('button[type="submit"]');
+  await page.click('.chicon-calendar');   
+  
+  await page.screenshot({ path: 'example.png' });
+  
+  await browser.close();
+})();
